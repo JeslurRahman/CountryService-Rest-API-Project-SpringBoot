6b0944913015ff5b407088ff4558c52e84dd5cbd
Jeslur Rahman <88666503+it20667696@users.noreply.github.com>
2024-01-23 09:51:37 +0530

    added the folder structure and implemted services class with HashMap

---
+++ src/main/java/com/countryservice/demo/services/CountryService.java
@@ -0,0 +1,91 @@
+package com.countryservice.demo.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.countryservice.demo.beans.Country;
+import com.countryservice.demo.controllers.AddResponse;
+
+public class CountryService {
+	
+	//HashMap
+	static HashMap<Integer, Country> countryIdMap;
+	
+	public CountryService()
+	{
+		countryIdMap = new HashMap<Integer, Country>();
+		
+		Country indiaCountry = new Country(1, "India", "Delhi");
+		Country usaCountry = new Country(2, "USA", "Washington");
+		Country ukCountry = new Country(3, "UK", "London");
+		
+		countryIdMap.put(1,indiaCountry);
+		countryIdMap.put(2,usaCountry);
+		countryIdMap.put(3,ukCountry);
+	}
+	
+	public List getAllCountries()
+	{
+		List countries = new ArrayList(countryIdMap.values());
+		return countries;
+	}
+	
+	public Country getCountryById(int id)
+	{
+		Country country = countryIdMap.get(id);
+		return country;
+	}
+	
+	public Country getCountryByName(String countryName)
+	{
+		Country country = null;
+		for(int i : countryIdMap.keySet())
+		{
+			if(countryIdMap.get(i).getCountryName().equals(countryName))
+			{
+				countryIdMap.get(i);
+			}
+		}	
+		return country;
+	}
+	
+	public Country addCountry(Country country)
+	{
+		country.setId(getMaxId());
+		countryIdMap.put(country.getId(), country);
+		return country;
+		
+	}
+	
+	//Utility method to get max id (generate ID automatically for each new record)
+	public static int getMaxId()
+	{
+		int max = 0;
+		for(int id:countryIdMap.keySet())
+		{
+			if(max <= id )
+				max = id;
+		}
+		return max + 1;
+	}
+	
+	public Country updateCountry(Country country)
+	{
+		if(country.getId() > 0)
+		{
+			countryIdMap.put(country.getId(), country);
+		}
+		return country;
+	}
+	
+	public AddResponse deleteCountry(int id)
+	{
+		countryIdMap.remove(id);
+		AddResponse res = new AddResponse();
+		res.setMsg("Country deleted...");
+		res.setId(id);
+		return res;
+	}
+
+}
