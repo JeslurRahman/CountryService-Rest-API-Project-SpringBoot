35f641afdd9ced9059a4113662924e662033e836
Jeslur Rahman <88666503+it20667696@users.noreply.github.com>
2024-01-27 12:32:12 +0530

    added H2 Database Mock Database for Unit/Integration Testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+Create table Country(id int primary key, country_name varchar(50), capital varchar(50));
+insert into Country values(1, 'India', 'Delhi');
+insert into Country values(2, 'China', 'Beijing');
+insert into Country values(3, 'Japan', 'Tokyo');
